e114d91857c56534a04ad4474ec576d6303a0cb1
Andrew Wong <andrewwong998@gmail.com>
2019-04-29 00:47:27 +0100

    Clean .zshenv

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -19,3 +19,11 @@ cd() {
 	builtin cd "$@" && l
 }
 
+# fd - cd to selected directory
+fd() {
+  local dir
+  dir=$(find ${1:-.} -path '*/\.*' -prune \
+                  -o -type d -print 2> /dev/null | fzf +m) &&
+  cd "$dir"
+}
+
