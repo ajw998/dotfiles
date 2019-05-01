1ae0a54c7188c32923e09e7727619ee7794b5c72
Andrew Wong <andrewwong998@gmail.com>
2019-05-01 23:24:44 +0100

    Change FZF settings

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -19,11 +19,3 @@ cd() {
 	builtin cd "$@" && l
 }
 
-# fd - cd to selected directory
-fd() {
-  local dir
-  dir=$(find ${1:-.} -path '*/\.*' -prune \
-                  -o -type d -print 2> /dev/null | fzf +m) &&
-  cd "$dir"
-}
-
