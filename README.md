3f447d2c9490600216c2c9c8304d6985827006aa
Andrew Wong <andrewwong998@gmail.com>
2019-05-08 20:29:45 +0100

    Define new functions

--- README.md
+++ README.md
@@ -6,6 +6,14 @@ Personal dotfiles for macOS and Ubuntu.
 
 * GNU Stow
 
+## Custom functions
+
+``fkill`` - Kill process
+
+``ss`` - Launch simple HTTP server
+
+``port`` - List ports-in-use
+
 ## Notes
 
 See VSCode settings [here](https://gist.github.com/ajw998/9eca8ae0422e1a56431c550598c9387f). 
--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -19,3 +19,35 @@ cd() {
 	builtin cd "$@" && l
 }
 
+# fkill - kill processes - list only the ones you can kill. Modified the earlier script.
+fkill() {
+    local pid
+    if [ "$UID" != "0" ]; then
+        pid=$(ps -f -u $UID | sed 1d | fzf -m | awk '{print $2}')
+    else
+        pid=$(ps -ef | sed 1d | fzf -m | awk '{print $2}')
+    fi
+
+    if [ "x$pid" != "x" ]
+    then
+        echo $pid | xargs kill -${1:-9}
+    fi
+}
+
+port() {
+    lsof -i -P -n | grep 'LISTEN' | fzf -m --ansi
+}
+
+# Launch simple server in Python
+# Defaults to port 8000
+# TODO: Check if port is available. If not, throw error
+ss() {
+    PORT=${1:-8000}
+    PYEXEC=$(which python)
+    if [ -z ${PYEXEC} ]; then
+        echo "Python not found, please ensure Python is available and try again."
+        exit 1
+    fi
+
+    python -m SimpleHTTPServer ${PORT}
+}
