96d241f1818d0ff3cfa7aaab41abb2a998f4d862
Andrew Wong <andrewwong998@gmail.com>
2020-04-20 21:38:29 +0100

    Add aliases for working with doom emacs

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,4 +1,3 @@
-# alias l='ls -Xlah --time-style=+"%Y-%m-%d" --color=auto -F'
 alias -g latest='*(om[1])'
 alias :q='exit'
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
@@ -8,8 +7,11 @@ alias l='exa -lah'
 alias mp='mpc toggle'
 alias o='open .'
 alias rf='source $HOME/.zshrc'
-alias t='task'
 alias tmux='tmux -2'
 alias update='sudo softwareupdate -i -a; brew upgrade; brew update; brew cleanup; brew cask upgrade; npm install npm -g; npm update -g; nvim +PlugUpdate +qall'
+# All vi-related stuff should map to neovim
+# Apart from vi (this is the fallback)
 alias v='nvim'
 alias vim='nvim'
+# Always run emacs in the background
+alias emacs='emacs &'
