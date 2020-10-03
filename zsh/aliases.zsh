bf1bcbceaedd19a873e940d4f87a31989ad76056
Andrew Wong <andrewwong998@gmail.com>
2020-10-03 12:10:27 +0100

    Add delhist

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -9,6 +9,7 @@ alias o='open .'
 alias rf='source $HOME/.zshrc'
 alias tmux='tmux -2'
 alias update='sudo softwareupdate -i -a; brew upgrade; brew update; brew cleanup; brew upgrade --cask; npm install npm -g; npm update -g; nvim +PlugUpdate +qall'
+alias delhist='find . -name "*history" -maxdepth 1 -print0 | xargs -0 /bin/rm -v'
 # All vi-related stuff should map to neovim
 # Apart from vi (this is the fallback)
 alias v='nvim'
